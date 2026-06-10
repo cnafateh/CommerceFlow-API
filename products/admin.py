d59278fb7012f70ac89749dcb051cadd148d86d0
Sina <sinafateh1382@gmail.com>
2026-06-10 14:57:09 +0330

    feat: register product models in admin

--- products/admin.py
+++ products/admin.py
@@ -10,24 +10,31 @@ class ProductImageInline(admin.TabularInline):
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "slug", "is_active"]
-    list_filter = ["is_active"]
-    search_fields = ["name", "slug"]
-    prepopulated_fields = {"slug": ["name"]}
+    list_display = ("id", "name", "slug", "is_active")
+    list_filter = ("is_active",)
+    search_fields = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
 
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "slug", "is_active"]
-    list_filter = ["is_active"]
-    search_fields = ["name", "slug"]
-    prepopulated_fields = {"slug": ["name"]}
+    list_display = ("id", "name", "slug", "is_active")
+    list_filter = ("is_active",)
+    search_fields = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "category", "brand", "price", "is_active"]
-    list_filter = ["is_active", "category", "brand"]
-    search_fields = ["name", "slug", "description"]
-    prepopulated_fields = {"slug": ["name"]}
+    list_display = (
+        "id",
+        "name",
+        "category",
+        "brand",
+        "price",
+        "is_active",
+    )
+    list_filter = ("is_active", "category", "brand")
+    search_fields = ("name", "slug", "description")
+    prepopulated_fields = {"slug": ("name",)}
     inlines = [ProductImageInline]
