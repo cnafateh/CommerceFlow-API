24c140dc410d5ba0f66c6f1b731d71bf28d55bee
Sina <sinafateh1382@gmail.com>
2026-06-10 15:05:04 +0330

    feat: add product read api

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductDetailSerializer, ProductListSerializer
+
+
+class ProductViewSet(ReadOnlyModelViewSet):
+    queryset = Product.objects.filter(is_active=True)
+    lookup_field = "slug"
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return ProductDetailSerializer
+        return ProductListSerializer
