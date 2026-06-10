e2707ffc8f92a09969b6ca85ce1a17573d0482f4
Sina <sinafateh1382@gmail.com>
2026-06-10 22:52:19 +0330

    feat: register order models in admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "status",
+        "total_price",
+        "created_at",
+    )
+    list_filter = ("status",)
+    search_fields = (
+        "id",
+        "user__username",
+        "user__email",
+    )
+    inlines = [OrderItemInline]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "order",
+        "product",
+        "quantity",
+        "price",
+    )
